d1120b58012dd7da1634ad4a1c0bc4d6631cd88c
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2025-11-25 00:03:34 -0500

    Remove duplicate file

--- Decursed/Source/General/Utility.cs
+++ Decursed/Source/General/Utility.cs
@@ -6,7 +6,6 @@ namespace Decursed;
 
 internal class Utility
 {
-	// Indexed by column (x) then row (y)
 	public static string[,] ParseCsv(string path, Point2 size)
 	{
 		var grid = new string[size.X, size.Y];
